4f360339fb66e63255f288c0fb69f969a0e55c32
bekkun <bekkunilme@tutye.com>
2020-01-30 22:03:07 +0400

    Add brain-prime game

---
+++ src/games/brain-prime.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+
+const brainPrime = () => {
+  const getRandomNum = (max, min) => Math.round(Math.random() * (max - min) + min);
+
+
+  const isPrime = (num) => {
+    if (num <= 0) {
+      return false;
+    }
+    const isDivide = num % num === 0 && num % 1 === 0;
+
+    if (num > 2 && isDivide) {
+      if (num % 2 === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let count = 0;
+
+  while (count < 3) {
+    const number = getRandomNum(0, 1000);
+    console.log(`Question: ${number}`);
+    const gamerAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+    if (gamerAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${gamerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}". Let's try again, ${name}`);
+      break;
+    }
+    count += 1;
+  }
+  if (count === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+export default brainPrime;
